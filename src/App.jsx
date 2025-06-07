ece2b68dd6fdaa46e06d49df0ebe1799e8befcb8
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2025-06-07 02:03:27 -0400

    modal testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, finalRef} from 'react'
 import axios from 'axios'
 import {
   ChakraProvider,
@@ -12,11 +12,33 @@ import {
   Box,
   Button,
   ButtonGroup,
-  Text
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input
+  
 
 } from '@chakra-ui/react'
 
 export function App() {
+  //modal code
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = useRef()
+  
+  const [editFirst, setEditFirst] = useState([])
+  const [editLast, setEditLast] = useState([])
+  const [editGender, setEditGender] = useState([])
+
+
+
   const [users, setUsers] = useState([])
   useEffect(() => {
     axios.get('http://localhost:3000/api/users')
@@ -34,6 +56,15 @@ export function App() {
       .catch(err => console.log(err))
   }
 
+  function handleEdit(id) {
+    axios.put(`http://localhost:3000/api/users/${id}`, {
+      first_name: editFirst,
+      last_name: editLast,
+      gender: editGender})
+  }
+
+
+  
   
   return (
     
@@ -45,7 +76,7 @@ export function App() {
       </ButtonGroup>
       <Box display="flex" justifyContent="center" mt={8}>
         <TableContainer>
-          <Table variant="simple" colorScheme="blue">
+          <Table variant="simple" colorScheme="grey">
             <Thead>
               <Tr>
                 <Th>id</Th>
@@ -64,8 +95,49 @@ export function App() {
                   <Td>{user.first_name}</Td>
                   <Td>{user.last_name}</Td>
                   <Td>{user.gender}</Td>
-                  <Td><Button  onClick={() => handleDelete(user._id)}>Edit</Button></Td>
-                  <Td><Button  onClick={() => handleDelete(user._id)}>Delete</Button></Td>
+                  <Td>
+                    
+                    <Button colorScheme='green' onClick={onOpen}>Edit</Button>
+                   
+                    <Modal
+                      initialFocusRef={initialRef}
+                      finalFocusRef={finalRef}
+                      isOpen={isOpen}
+                      onClose={onClose}
+                    >
+                      <ModalOverlay />
+                      <ModalContent>
+                        <ModalHeader>Edit User</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={6}>
+                          <FormControl>
+                            <FormLabel>First name</FormLabel>
+                            <Input  value = {editFirst} ref={initialRef}  placeholder='First name' onChange={e => setEditFirst(e.target.value)} />
+                          </FormControl>
+
+                          <FormControl mt={4}>
+                            <FormLabel>Last name</FormLabel>
+                            <Input  value = {editLast} placeholder='Last name' onChange={e => setEditLast(e.target.value)} />
+                          </FormControl>
+
+                          <FormControl mt={4}>
+                            <FormLabel>Gender</FormLabel>
+                            <Input value = {editGender} placeholder='Last name' onChange={e => setEditGender(e.target.value)} />
+                          </FormControl>
+
+                        </ModalBody>
+
+                        <ModalFooter>
+                          <Button onClick={() => handleEdit(user._id)} colorScheme='blue' mr={3}>
+                            Save
+                          </Button>
+                          <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                      </ModalContent>
+                    </Modal>
+                
+                  </Td>
+                  <Td><Button  colorScheme="red" onClick={() => handleDelete(user._id)}>Delete</Button></Td>
                   <Td><Button colorScheme="teal" onClick={() => window.location.href = `/${user._id}`}>Page</Button></Td>
                 </Tr>
               ))}
@@ -76,4 +148,5 @@ export function App() {
     </ChakraProvider>
    
   )
+
 }
